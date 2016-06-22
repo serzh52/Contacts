73b754819cc27846cdf6362c05ed8478bc928c72
Sergey <serzh52@list.ru>
2016-06-22 16:17:49 +0300

    rew

--- app/src/main/java/com/example/sergey/contacts/HomeActivity.java
+++ app/src/main/java/com/example/sergey/contacts/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.sergey.contacts;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.TabLayout;
@@ -13,36 +17,67 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.sergey.contacts.adapter.TabAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    private ListView lstNames;
     FragmentManager fragmentManager;
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        this.lstNames = (ListView) findViewById(R.id.lstNames);
+        showContacts();
+
         fragmentManager = getSupportFragmentManager();
 
         setUi();
+    }
 
-        ListView contactsList = (ListView) findViewById(R.id.contactList);
-        ArrayList<String> contacts = new ArrayList<String>();
-
-        Cursor contactsCursor = getContentResolver()
-                .query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        while (contactsCursor.moveToNext()) {
-            String contact = contactsCursor.getString(
-                    contactsCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
-            contacts.add(contact);
+    private void showContacts() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+        } else {
+            List<String> contacts = getContactNames();
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contacts);
+            lstNames.setAdapter(adapter);
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, contacts);
-        contactsList.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showContacts();
+            } else {
+                Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private List<String> getContactNames() {
+        List<String> contacts = new ArrayList<>();
+        ContentResolver cr = getContentResolver();
+        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                contacts.add(name);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
+        return contacts;
     }
 
     private void setUi() {
@@ -84,4 +119,5 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(HomeActivity.this, AddContactActivity.class);
         startActivity(intent);
     }
+
 }
